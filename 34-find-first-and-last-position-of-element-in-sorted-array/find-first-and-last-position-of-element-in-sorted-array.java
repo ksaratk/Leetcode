b88994f0033eaababb68be19ad88707896797018
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-09-12 19:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (79.04%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -8,6 +8,7 @@ class Solution {
             int high = nums.length-1;
         while(low <= high){
         int mid = (low + high) / 2;
+
         if(nums[mid] == target){
             first = mid;
             high = mid-1;
@@ -30,8 +31,7 @@ class Solution {
         }else{
            low = mid + 1;
         }
-     }
-
+    }
      return new int[]{first,last};
     }
 }
