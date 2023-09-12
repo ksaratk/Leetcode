a7c839953633e35a2f683ffb34ee5953a2e70159
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-09-12 19:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (79.04%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
             
-            int [] result = new int[2];
-            int first = firstOccurance(nums, target);
-            int last = lastOccurance(nums, target);
-            result[0] = first;
-            result[1] = last;
-            return result;
-    }
+            int first = -1;
+            int last  = -1;
 
-    public int firstOccurance(int[] nums, int target){
-        int low=0;
-        int high=nums.length-1;
-        int ans =-1;
-        while(low<=high){
-        int mid = (low+high)/2;
-        if(nums[mid]==target){
-            ans = mid;
+            int low = 0;
+            int high = nums.length-1;
+        while(low <= high){
+        int mid = (low + high) / 2;
+        if(nums[mid] == target){
+            first = mid;
             high = mid-1;
-        } else if(nums[mid]>target){
-           high = mid-1;
+        } else if(nums[mid] > target){
+            high = mid-1;
         }else{
-           low = mid+1;
+           low = mid + 1;
         }
-        }
-        return ans;
     }
-    public int lastOccurance(int[] nums, int target){
-         int low=0;
-        int high=nums.length-1;
-        int ans =-1;
-        while(low<=high){
-        int mid = (low+high)/2;
-        if(nums[mid]==target){
-            ans = mid;
-            low = mid+1;
-        } else if(nums[mid]>target){
-           high = mid-1;
+            low = 0;
+            high = nums.length-1;
+
+        while(low <= high){
+        int mid = (low + high)/2;
+        if(nums[mid] == target){
+            last = mid;
+            low = mid + 1;
+        } else if(nums[mid] > target){
+           high = mid - 1;
         }else{
-           low = mid+1;
+           low = mid + 1;
         }
-        }
-        return ans;
+     }
+
+     return new int[]{first,last};
     }
 }
