9273a9acd7842ca519e9774d810f968fc0fb3df7
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-09-25 14:26:16 +0530

    Time: 1 ms (61.83%) | Memory: 40.7 MB (9.19%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,46 +1,45 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        
-        int n = nums.length;
         int low = Integer.MIN_VALUE;
-        int high =0;
-        // if(k==1){
-        //     return high;
-        // }
-        for(int i=0; i<n; i++){
-            high = high+nums[i];
+        int high = 0;
+
+        for(int i=0; i<nums.length; i++){
+            high += nums[i];
             low = Math.max(low, nums[i]);
         }
-            // for(int i=low; i<=high; i++){
-            //     if(subArraySum(nums, i)==k){
-            //         return i;
-            //     }
-            // }
-            // return low;
 
-            while(low<=high){
-                int mid = (low+high)/2;
-                int subArray = subArraySum(nums, mid);
-                if(subArray>k){
-                    low = mid+1;
-                } else{
-                    high = mid - 1;
-                }
+        // for(int i = max; i<=sum; i++){
+        //     int count = countSubArray(nums, i);
+        //     if(count == k){
+        //         return i;
+        //     }
+        // }
+        // return max;
+        int ans = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int count = countSubArray(nums, mid);
+            if(count>k){
+                low = mid+1;
+            }else{
+                high = mid - 1;
             }
-            return low;
+        }
+        return low;
     }
 
-    public int subArraySum(int [] nums, int number){
-        int subArray = 1;
-        int sum = 0;
+    public int countSubArray(int [] nums, int sum){
+        int subArrayCount = 1;
+        int value = 0;
+
         for(int i=0; i<nums.length; i++){
-            if(sum + nums[i]<=number){
-            sum+= nums[i];
-        } else{
-            subArray++;
-            sum = nums[i];
+            if(value+nums[i]<=sum){
+                value+=nums[i];
+            }else{
+               subArrayCount++;
+               value = nums[i]; 
+            }
         }
-    }
-    return subArray;
+        return subArrayCount;
     }
 }
