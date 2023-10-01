35bf8ee8b619b0f28e9d4fa720fe9fa96c29135f
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-10-01 23:54:13 +0530

    Time: 26 ms (41.32%) | Memory: 44.3 MB (30.74%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,44 +1,33 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        
-        int n = piles.length;
-        int maxElement = findMax(piles, n);
-        // for(int i = 1; i<maxElement; i++){
-        //     int requiredTime = calculateHour(piles, i, n);
-        //     if (requiredTime <= h){
-        //       return i;
+        int low  = 1;
+        int high = Integer.MIN_VALUE;
+        for(int i=0; i<piles.length; i++){
+            high = Math.max(high, piles[i]);
+        }
+        // for(int i=low; low<=high; i++){
+        //     if(possible(piles,i)<=h){
+        //         return i;
         //     }
-        //   }
-        int low = 1;
-        int high = maxElement;
-        int ans = 0;
-        while(low <= high){
-            int mid = (low + high)/2;
-            int requiredTime = calculateHour(piles, mid, n);
-            if (requiredTime <= h){
+        // }
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            int value = possible(piles,mid);
+            if(value<=h){
                 high = mid-1;
             } else{
-                low  = mid+1;
+                low = mid +1;
             }
         }
-          return low;
-        }
+        return low;
+    }
 
-        public int findMax(int [] pile, int n){
-        int max = Integer.MIN_VALUE;
-        
-            for(int i=0; i<n; i++){
-                max = Math.max(max, pile[i]);
-            }
-            return max;
+    public int possible(int [] piles, int time){
+         int totalTime = 0;
+        for(int i=0; i<piles.length; i++){
+            totalTime += Math.ceil((double)piles[i]/(double)time);
         }
-
-        public int calculateHour(int [] pile, int hour, int n){
-        int requiredTime = 0;
-
-            for(int i=0; i<n; i++){
-                requiredTime += Math.ceil((double)pile[i]/(double)hour);
-            }
-            return requiredTime;
-        }
+        return totalTime;
+    }
 }
