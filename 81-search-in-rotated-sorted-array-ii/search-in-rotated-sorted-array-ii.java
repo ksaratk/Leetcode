53aae7307544cf9876ad8618ec7aeb5e32661680
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-10-01 22:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (36.07%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,34 @@
 class Solution {
-    public boolean search(int[] arr, int k) {
-        int low = 0;
-        int high = arr.length - 1;
+    public boolean search(int[] nums, int target) {
+         int low = 0;
+        int high = nums.length-1;
 
         while(low<=high){
-            
-            int mid = ( low + high )/ 2;
-            if( arr[mid] == k ){
+            int mid  = (low+high)/2;
+
+            if(nums[mid]==target){
                 return true;
+            } 
+            
+            if(nums[low]==nums[mid] && nums[high]==nums[mid]){
+                low = low+1;
+                high = high-1;
+                continue;
             }
-            if( arr[low] == arr[mid] && arr[mid] == arr[high] ){
-                  low = low + 1;
-                  high = high - 1;
-                  continue;
-            }
-            if( arr[low] <= arr[mid] ){
-                if( arr[low] <= k && k <= arr[mid] ){
-                    high = mid - 1;
-                } else{
-                    low = mid + 1;
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<=nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
                 }
             } else{
-                if( arr[mid] <= k && k <= arr[high] ){
-                    low = mid + 1;
-                } else{
-                    high = mid - 1;
+                if(target>=nums[mid] && target<=nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
                 }
             }
         }
-            return false;
+        return false;
     }
 }
