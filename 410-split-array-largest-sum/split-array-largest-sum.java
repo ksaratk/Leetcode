fd24ff1a6132d67dd0bbb6252667b4d20dbc4aaf
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-10-07 21:09:38 +0530

    Time: 1 ms (62.34%) | Memory: 40.7 MB (12.25%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,45 +1,41 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int low = Integer.MIN_VALUE;
-        int high = 0;
-
+        
+        int min = Integer.MIN_VALUE;
+        int max = 0;
         for(int i=0; i<nums.length; i++){
-            high += nums[i];
-            low = Math.max(low, nums[i]);
+            max += nums[i];
+            min = Math.max(min, nums[i]); 
         }
-
-        // for(int i = max; i<=sum; i++){
-        //     int count = countSubArray(nums, i);
-        //     if(count == k){
-        //         return i;
-        //     }
-        // }
-        // return max;
-        int ans = 0;
+        int low = min;
+        int high = max;
         while(low<=high){
             int mid = (low+high)/2;
-            int count = countSubArray(nums, mid);
-            if(count>k){
-                low = mid+1;
+            int result = isPossible(nums, mid);
+            // if(result==k){
+            //     return mid;
+            // } 
+            if(result<=k){
+                high = mid-1;
             }else{
-                high = mid - 1;
+                low = mid+1;
             }
         }
         return low;
-    }
+        }
 
-    public int countSubArray(int [] nums, int sum){
-        int subArrayCount = 1;
-        int value = 0;
+    public int isPossible(int [] nums, int sum){
+        int totalSum = 0;
+        int subArray = 1;
 
         for(int i=0; i<nums.length; i++){
-            if(value+nums[i]<=sum){
-                value+=nums[i];
-            }else{
-               subArrayCount++;
-               value = nums[i]; 
+            if(totalSum+nums[i]<=sum){
+                totalSum += nums[i];
+            } else{
+                subArray++;
+                totalSum = nums[i];
             }
         }
-        return subArrayCount;
+        return subArray;
     }
 }
