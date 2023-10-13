80293f74f1f9383eb78d4983053fb6b14ba5287f
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-10-13 23:17:47 +0530

    Time: 1 ms (89.65%) | Memory: 43.5 MB (42.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-		int close = n;
-		int open = n;
-		List<String> list = new ArrayList<>();
-		String op = "";
-	  return balanceParenthisis(open, close, list, op);
+				int open = n;
+				int close = n;
+				List<String> result = new ArrayList();
+        helper(open, close, "", result);
+        return result;
 	}
-	
-	public List<String> balanceParenthisis(int open, int close, List<String> list, String op){
-		
-		if(open==0 && close==0) {
-		   list.add(op);
-		   return list;
-		}
-		
-		if(open!=0) {
-			String op1 = op;
-			op1 =  op+"(";
-			balanceParenthisis(open-1, close, list, op1);
-		} if(close>open) {
-			String op2 = op;
-			op2 =  op+")";
-			balanceParenthisis(open, close-1, list, op2);
-		}
-		return list;
+
+	private void helper(int open, int close, String string, List<String> list) {
+		// TODO Auto-generated method stub
+			if(open==0 && close ==0) {
+				list.add(string);
+				return;
+			}
+			
+			if(open!=0) {
+				String st1 = string;
+				st1 += "(";
+				helper(open-1, close, st1, list);
+			}
+			if(open<close) {
+				String st2 = string;
+				st2 += ")";
+				helper(open, close-1, st2, list);
+			}
 	}
 }
