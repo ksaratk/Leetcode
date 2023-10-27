34630bba9c7e58bd7abcef39bb375910edd0a6cd
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-10-27 14:04:58 +0530

    Time: 194 ms (5.46%) | Memory: 44.6 MB (24.00%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,46 @@
 class Solution {
     public int trap(int[] height) {
         int count = 0;
-        for(int i=0; i<height.length; i++){
-            count += Math.min(leftMax(height, i), rightMax(height, i)) - height[i];
+        int n = height.length;
+        int [] leftMax = new int[n];
+        int [] rightMax = new int[n];
+        for(int a:leftMax){
+            System.out.print(a+" ");
+        }
+        for(int a:rightMax){
+            System.out.print(a+" ");
+        }
+        leftMaxInsert(leftMax, height, n);
+        rightMaxInsert(rightMax, height, n);
+        for(int i=0; i<n; i++){
+            count += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
         return count;
     }
 
-    public int leftMax(int [] height, int j){
+    public void leftMaxInsert(int [] leftMax, int [] height, int n){
         int max = Integer.MIN_VALUE;
 
-        for(int i = j; i>=0; i--){
-            max = Math.max(max, height[i]); 
-        }    
-        return max;    
+        for(int i = 0; i<n; i++){
+            if(height[i]>max){
+                max = height[i];
+                leftMax[i] = max;
+            }  else{
+                leftMax[i] = max;
+            }     
+        }        
     }
 
-    public int rightMax(int [] height, int j){
-        int max = Integer.MIN_VALUE;
+    public void rightMaxInsert(int [] rightMax, int [] height, int n){
+         int max = Integer.MIN_VALUE;
 
-        for(int i = j; i<height.length; i++){
-            max = Math.max(max, height[i]); 
-        }    
-        return max;    
+        for(int i = n-1; i>=0; i--){
+            if(height[i]>max){
+                max = height[i];
+                rightMax[i] = max;
+            } else{
+                rightMax[i] = max;
+            }       
+        }        
     }
 }
