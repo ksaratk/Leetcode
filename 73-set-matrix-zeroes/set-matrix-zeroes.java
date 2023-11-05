6eb0814550cbfff330f35a652f3fffe769d6bc61
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-11-05 22:43:10 +0530

    Time: 1 ms (89.69%) | Memory: 44.8 MB (21.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,23 +3,39 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-       int [] row = new int [n];
-       int [] col = new int [m];
+       int col = 1;
 
        for(int i=0; i<n; i++){
-           for(int j=0; j<m; j++){
+           for(int j=0; j<m; j++){   
                if(matrix[i][j]==0){
-                   row[i] = 1;
-                   col[j] = 1;
+                   matrix[i][0] = 0;
+
+                  if(j!=0){
+                       matrix[0][j] = 0;
+                  }else{
+                      col=0;
+                  }
                }
            }
        }
 
-       for(int i=0; i<n; i++){
-           for(int j=0; j<m; j++){
-               if(row[i]==1 || col[j]==1){
+       for(int i=1; i<n; i++){
+           for(int j=1; j<m; j++){
+               if(matrix[i][0]==0 || matrix[0][j]==0){
                    matrix[i][j] = 0;
                }
+           }
+       }
+
+       if(matrix[0][0]==0){
+           for(int i=0; i<m; i++){
+               matrix[0][i] = 0;
+           }
+       }
+
+       if(col==0){
+           for(int i=0; i<n; i++){
+               matrix[i][0] = 0;
            }
        }
     }
