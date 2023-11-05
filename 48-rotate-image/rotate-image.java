0fca086ba776675f10fa5f5ce9421e2ae1c6e281
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-11-05 23:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (40.60%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -6,7 +6,7 @@ class Solution {
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                result[j][n-i-1] = matrix[i][j];
+                result[j][m-i-1] = matrix[i][j];
             }
         }
 
