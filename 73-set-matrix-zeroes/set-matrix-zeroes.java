d58760a32be7e92e0ece83e6df1281f6695e4e90
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-11-05 22:14:50 +0530

    Time: 1 ms (89.69%) | Memory: 44.6 MB (33.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,31 +2,25 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int marker = Integer.MIN_VALUE+9;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==0){
-                    for(int k = 0; k<n; k++){
-                        if(matrix[k][j]!=0){
-                            matrix[k][j] = marker;
-                        }
-                    }
 
-                    for(int k = 0; k<m; k++){
-                        if(matrix[i][k]!=0){
-                            matrix[i][k] = marker;
-                        }
-                    }
-                }
-            }
-        }
+       int [] row = new int [n];
+       int [] col = new int [m];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==marker){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+       for(int i=0; i<n; i++){
+           for(int j=0; j<m; j++){
+               if(matrix[i][j]==0){
+                   row[i] = 1;
+                   col[j] = 1;
+               }
+           }
+       }
+
+       for(int i=0; i<n; i++){
+           for(int j=0; j<m; j++){
+               if(row[i]==1 || col[j]==1){
+                   matrix[i][j] = 0;
+               }
+           }
+       }
     }
 }
