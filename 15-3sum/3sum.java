672748cb00a96018106efc699de0e3058a990e41
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-11-06 21:47:00 +0530

    Time: 34 ms (45.53%) | Memory: 50.2 MB (83.88%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,22 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet();
-        Set<Integer> tempSet = null;
-        for(int i=0; i<nums.length; i++){
-            int sum =0;
-            tempSet = new HashSet<>();
-            for(int j=i+1; j<nums.length; j++){
-                 sum = -(nums[i]+nums[j]);
-                 if(tempSet.contains(sum)){
-                     List<Integer> tempList = Arrays.asList(nums[i], nums[j], sum);
-                     tempList.sort(null);
-                     set.add(tempList);
-                 }else{
-                     tempSet.add(nums[j]);
-                 }          
-            }
-        }
-        List<List<Integer>> list = new ArrayList<>(set);
-        return list;
+        Set<List<Integer>> set = new HashSet<>();
+       Arrays.sort(nums);
+       int n = nums.length;
+       for(int i=0; i<nums.length; i++){
+
+            if(i>0 && nums[i-1]==nums[i]) continue;
+           int j = i+1;
+           int k = n-1;
+
+           while(j<k){
+               int sum = nums[i]+nums[j]+nums[k];
+
+               if(sum>0){
+                 k--;
+               } else if(sum<0){
+                   j++;
+               } else{
+                   List<Integer> tempList = Arrays.asList(nums[i],nums[j],nums[k]);
+                   tempList.sort(null);
+                   set.add(tempList);
+                    k--;
+                    j++;
+
+                    while(nums[j]==nums[j-1] && j<k) j++;
+                    while(nums[k]==nums[k+1] && j<k) k--;
+               }
+           }
+       }
+       List<List<Integer>> list = new ArrayList<>(set);
+       return list;
     }
 }
