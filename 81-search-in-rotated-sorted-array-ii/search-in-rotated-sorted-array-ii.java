527263c0cd8266ceba6a8eb7f5be521866c6d292
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-11-07 19:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (5.86%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,34 +1,38 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-         int low = 0;
-        int high = nums.length-1;
+         boolean ans = false;
+        int n = nums.length-1;
+        int left = 0;
+        int right = n;
 
-        while(low<=high){
-            int mid  = (low+high)/2;
+        while(left<=right){
+            int mid = (right+left)/2;
 
-            if(nums[mid]==target){
-                return true;
-            } 
-            
-            if(nums[low]==nums[mid] && nums[high]==nums[mid]){
-                low = low+1;
-                high = high-1;
-                continue;
-            }
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<=nums[mid]){
-                    high = mid-1;
-                }else{
-                    low = mid+1;
-                }
-            } else{
-                if(target>=nums[mid] && target<=nums[high]){
-                    low = mid+1;
-                }else{
-                    high = mid-1;
-                }
-            }
+            if(nums[mid] == target){
+                ans = true;
+                break;
+            }  
+
+            if(nums[left] == nums[mid] && nums[mid] == nums[right]) {
+            left = left+1;
+                right = right-1;
+            continue;
         }
-        return false;
+
+            if(nums[left] <= nums[mid]){
+                if(target>= nums[left] && target<= nums[mid]){
+                right = mid-1;
+            } else{
+                left = mid+1;
+            }
+        } else{
+            if(target>= nums[mid] && target<= nums[right]){
+                left = mid+1;
+            } else{
+                right = mid-1;
+            }
+        } 
+    }
+    return ans;
     }
 }
