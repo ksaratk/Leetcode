928e8279b1d3174daa741e4e078c88fcfcac57bd
ksaratk <144666810+ksaratk@users.noreply.github.com>
2023-11-21 11:32:25 +0530

    Time: 15 ms (14.22%) | Memory: 54.6 MB (72.79%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,65 +9,23 @@
  * }
  */
 class Solution {
-
-    public ListNode findMid(ListNode head){
-        ListNode fast = head.next;
-        ListNode slow = head;
-
-        while(fast!=null && fast.next!=null){
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
     public ListNode sortList(ListNode head) {
-       if(head==null || head.next==null){
-           return head;
-       }
 
-       ListNode mid = findMid(head);
+        List<Integer> list = new ArrayList();
 
-       ListNode left = head;
-       ListNode right = mid.next;
-       mid.next = null; 
+         ListNode temp = head;
+         while(temp!=null){
+             list.add(temp.val);
+             temp = temp.next;
+         }
 
-       left = sortList(left);
-       right = sortList(right);
-       ListNode result = mergeSort(left, right);
-       return result;
-    }
- 
-    public ListNode mergeSort(ListNode left, ListNode right){
-        if(left==null){
-            return left;
-        }else if(right == null){
-            return right;
-        }
-        ListNode ans = new ListNode(-1);
-        ListNode temp = ans;
-        while(left!=null && right!=null){
-            if(left.val < right.val){
-                temp.next = left;
-                left = left.next;
-                temp = temp.next;
-            } else{
-                temp.next = right;
-                right = right.next;
-                temp = temp.next;
-            }
-        }
-
-        while(left!=null){
-                temp.next = left;
-                left = left.next;
-                temp = temp.next;
-        }
-
-        while(right!=null){
-                temp.next = right;
-                right = right.next;
-                temp = temp.next;
-        }
-        return ans = ans.next;
+         Collections.sort(list);
+        ListNode newNode = new ListNode(0);
+        ListNode temp2 = newNode;
+         for(int num : list){
+             temp2.next = new ListNode(num);
+             temp2 = temp2.next;
+         }
+         return newNode.next;
     }
 }
