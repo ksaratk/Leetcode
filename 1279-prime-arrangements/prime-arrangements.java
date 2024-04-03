c2c6e8a79ad67b2819e25768c2d069aec339f623
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-03 16:23:35 +0530

    Time: 1 ms (36.41%) | Memory: 40.3 MB (50.00%) - LeetSync

---
+++ 1279-prime-arrangements/prime-arrangements.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static final int mod = 1000000007;
+    public int numPrimeArrangements(int n) {
+        int prime = 0;
+        for(int i = 1; i<=n; i++){
+            if(isPrime(i) == true){
+                prime++;
+            }
+        }
+    int nonP = n - prime;
+    long result = (factorial(prime) * factorial(nonP))%mod;
+    return (int)result;
+    }
+
+    public long factorial(int x){
+        long fact = 1;
+        for(int i = 2; i<=x; i++){
+            fact = (fact * i)% mod;
+        }
+        return fact;
+    }
+
+    public boolean isPrime(int x){
+        if(x<=1) return false;
+        for(int i=2; i<x; i++){
+            if(x%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
