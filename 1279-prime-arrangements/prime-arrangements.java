ba0f28cc518be2fb8990a8f3b884452d6ddc8ad9
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-03 16:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (71.36%) - LeetSync

--- 1279-prime-arrangements/prime-arrangements.java
+++ 1279-prime-arrangements/prime-arrangements.java
@@ -22,7 +22,7 @@ class Solution {
 
     public boolean isPrime(int x){
         if(x<=1) return false;
-        for(int i=2; i<x; i++){
+        for(int i=2; i*i<=x; i++){
             if(x%i == 0){
                 return false;
             }
