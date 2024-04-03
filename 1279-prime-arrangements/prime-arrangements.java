63c5c4f51d7e3e3280f525e42adbce1de8ecad3d
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-03 16:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (50.00%) - LeetSync

--- 1279-prime-arrangements/prime-arrangements.java
+++ 1279-prime-arrangements/prime-arrangements.java
@@ -1,32 +1,41 @@
 class Solution {
     public static final int mod = 1000000007;
     public int numPrimeArrangements(int n) {
-        int prime = 0;
-        for(int i = 1; i<=n; i++){
-            if(isPrime(i) == true){
-                prime++;
-            }
-        }
-    int nonP = n - prime;
-    long result = (factorial(prime) * factorial(nonP))%mod;
-    return (int)result;
+        int primeNumber = getPrime(n);
+        int nonPrime = n - primeNumber;
+        long primeNumberFact = fact(primeNumber);
+        long nonPrimeNumberFact = fact(nonPrime);
+        long result = (primeNumberFact * nonPrimeNumberFact)%mod;
+        return (int)result;
     }
 
-    public long factorial(int x){
-        long fact = 1;
-        for(int i = 2; i<=x; i++){
-            fact = (fact * i)% mod;
-        }
-        return fact;
-    }
+    public int getPrime(int n){
+        boolean [] prime = new boolean[n+1];
 
-    public boolean isPrime(int x){
-        if(x<=1) return false;
-        for(int i=2; i*i<=x; i++){
-            if(x%i == 0){
-                return false;
+        Arrays.fill(prime, true);
+
+        for(int i = 2; i*i<=n; i++){
+            if(prime[i]){
+                for(int j = i*i; j<=n; j = j+i){
+                    prime[j] = false;
+                }
             }
         }
-        return true;
+
+        int primeCount = 0;
+        for(int i = 2; i<=n; i++){
+            if(prime[i] == true){
+                primeCount++;
+            }
+        }
+        return primeCount;
+    }
+
+    public long fact(int n){
+        long factorial = 1;
+        for(int i=2; i<=n; i++){
+            factorial = (factorial * i)%mod;
+        }
+        return factorial;
     }
 }
