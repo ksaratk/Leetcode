98208304a51a5f2b42d5d6d6608371fe663dc15a
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-05 23:32:15 +0530

    Time: 2 ms (100.00%) | Memory: 58.1 MB (31.00%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,15 +1,22 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int result [] = new int[queries.length];
-        for(int i=0; i<queries.length; i++){
-          int left = queries[i][0];
-          int right = queries[i][1];
-            int xor = 0;
-            for(int j = left; j<=right; j++){
-                xor ^= arr[j];
-            }
-            result[i] = xor;
+        int [] prifixXor = new int [arr.length];
+        int [] ans = new int [queries.length];
+         prifixXor[0] = arr[0];
+        for(int i=1; i<prifixXor.length; i++){ 
+            prifixXor [i] = prifixXor[i-1]^arr[i];
         }
-        return result;
+            
+            for(int i = 0; i<queries.length; i++){
+                int left = queries[i][0];
+                int right = queries[i][1];
+
+                if(left == 0){
+                    ans[i] = prifixXor[right];
+                } else{
+                    ans[i] =  prifixXor[right] ^ prifixXor[left-1];
+                }
+            }
+        return ans;
     }
 }
