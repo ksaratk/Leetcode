b736fc815b930d6a617cabc24b25a533c15f620c
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-06 05:44:05 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (25.90%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,32 +1,29 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-
-        int[] arr=new int[2];
-
-        HashMap<Integer, Integer> hm= new HashMap<Integer, Integer>();
-        // key:value
+        //Find their XOR
+        int result = 0;
         for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])){
-                //update the value to +1
-                hm.put(nums[i],hm.get(nums[i])+1);
-
+            result ^= nums[i];
+        }
+        //Finding a SetBit
+        int setBit = 0;
+        while((result & (1 << setBit)) == 0){
+            setBit++;
+        }
+        int ans1 = 0;
+        int ans2 = 0; 
+        for(int i=0;i<nums.length;i++){
+            if((nums[i] & (1 << setBit)) != 0){
+                ans1 ^= nums[i];
             }
             else{
-                hm.put(nums[i],1);
+                ans2 ^= nums[i];
             }
         }
-
-        int index=0;
-        for(Map.Entry<Integer, Integer> entry:hm.entrySet()){
-            if(entry.getValue()==1){
-
-             arr[index++]= entry.getKey();
-             if(index==2) break;
-
-        }
-        }
-
-    return arr;
-
+        int[] ans = new int[2];
+        ans[0] = ans1;
+        ans[1] = ans2;
+        Arrays.sort(ans);
+        return ans;
     }
 }
