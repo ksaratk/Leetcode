1bb7d8c8f32820dd0d284a7b7040434fc4bce3fa
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-15 23:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (21.28%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.java
+++ 2630-alternating-digit-sum/alternating-digit-sum.java
@@ -1,30 +1,22 @@
 class Solution {
     public int alternateDigitSum(int n) {
-        int sum = 0;
-        int no = reverse(n);
-        int treaker = 0;
-        while(no>0){
-            int digit = no%10;
-            if(treaker == 0){
-                sum += digit;
-                treaker = 1;
-            } else{
-                sum += -(digit);
-                treaker = 0;
-            }
-            no/=10;
-        }
-
-        return sum;
-    }
-
-    public int reverse(int n){
-        int no = 0;
-
+        
+        int reverse = 0;
         while(n>0){
-            no = no*10 + n%10;
-            n/=10;
+            reverse = reverse*10 + n%10;
+            n = n/10;
         }
-        return no;
+        int flag = 0;
+        int result = 0;
+        while(reverse>0){
+            if(flag == 0){
+                result += reverse%10;
+            }else{
+                result = result+(-1*reverse%10);
+            }
+            reverse = reverse/10;
+            flag = 1-flag;
+        }
+        return result;
     }
 }
