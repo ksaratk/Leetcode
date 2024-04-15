9bb7a4196ab10d44cb8cb6f6998974b5f51991a1
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-15 20:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (95.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0 || n == 1) return n;
-
-       return fib(n-1) + fib(n-2); 
+        if(n==0 || n==1) return n;
+        int a = 0;
+        int b = 1;
+        for(int i=2; i<=n; i++){
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 }
