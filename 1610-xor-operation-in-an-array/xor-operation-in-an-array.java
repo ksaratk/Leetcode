e1e7c60531e38f4709231f3fefee30b991ab9bc1
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-16 01:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (38.53%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
@@ -1,14 +1,9 @@
 class Solution {
     public int xorOperation(int n, int start) {
-        
-        int [] ar = new int [n];
-
+        // int [] nums = new int [n];
+    	int xor = 0;
         for(int i=0; i<n; i++){
-            ar[i] = start + 2 * i;
-        }
-        int xor = 0;
-        for(int i=0; i<n; i++){
-            xor ^= ar[i];
+            xor = xor^(start + 2 * i);
         }
         return xor;
     }
