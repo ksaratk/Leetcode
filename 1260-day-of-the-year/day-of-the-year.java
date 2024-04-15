7947f6982098a900a33cd633685eaa2f204cf327
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-15 22:40:46 +0530

    Time: 11 ms (26.77%) | Memory: 45.2 MB (48.35%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.java
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -1,22 +1,18 @@
 class Solution {
+    int [] months = {31,28,31,30,31,30,31,31,30,31,30,31}; 
     public int dayOfYear(String date) {
-        int [] days = new int []{31,28,31,30,31,30,31,31,30,31,30,31};
-
         String [] ar = date.split("-");
-        int year =  Integer.parseInt(ar[0]);
+        int year = Integer.parseInt(ar[0]);
         int month = Integer.parseInt(ar[1]);
-        int day =   Integer.parseInt(ar[2]);
-        
-        int result = day;
-        
-        for(int i=0; i< month-1; i++){
-            result = result + days[i];
-        }
-        System.out.println(result);
+        int days = Integer.parseInt(ar[2]);
 
-        if(month>2 && year%4 ==0 && (year%400 == 0 || year%100 != 0)){
-            result += 1;
+        int day = days;
+        if(month>2 && year%4 == 0 && (year%400 ==0 || year%100 != 0)){
+            day = day+1;
         }
-        return result;
+        for(int i=0; i<month-1; i++){
+            day += months[i];
+        }
+        return  day;
     }
 }
