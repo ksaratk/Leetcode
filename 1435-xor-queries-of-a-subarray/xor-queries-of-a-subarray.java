8ed59316bfcda3e4a31d4f2aedd630ac4be1750a
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-17 00:08:47 +0530

    Time: 2 ms (100.00%) | Memory: 57.3 MB (67.56%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int[] xorQueries(int[] arr, int[][] queries) {
-        int [] prifixXor = new int [arr.length];
+        public int[] xorQueries(int[] arr, int[][] queries) {
+        int [] prifix = new int[arr.length];
         int [] ans = new int [queries.length];
-         prifixXor[0] = arr[0];
-        for(int i=1; i<prifixXor.length; i++){ 
-            prifixXor [i] = prifixXor[i-1]^arr[i];
+        prifix[0] = arr[0];
+        for(int i = 1; i<prifix.length; i++){
+            prifix[i] = prifix[i-1]^arr[i];
         }
-            
-            for(int i = 0; i<queries.length; i++){
-                int left = queries[i][0];
-                int right = queries[i][1];
+        for(int i=0; i<queries.length; i++){
+            int a = queries[i][0];
+            int b = queries[i][1];
 
-                if(left == 0){
-                    ans[i] = prifixXor[right];
-                } else{
-                    ans[i] =  prifixXor[right] ^ prifixXor[left-1];
-                }
+            if(a == 0){
+                ans[i] = prifix[b];
+            } else{
+                ans[i] = prifix[b]^prifix[a-1]; 
             }
+        }
         return ans;
     }
 }
