45c197c931e977d77e7bf89cb214c2183ce09984
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-16 15:04:58 +0530

    Time: 62 ms (15.57%) | Memory: 40.1 MB (93.71%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,39 +1,37 @@
 class Solution {
-    boolean primes [] = new boolean[33];
+     boolean [] prime = new boolean[33];
     public int countPrimeSetBits(int left, int right) {
+        Arrays.fill(prime, true);
         int count = 0;
-        int ans = 0;
-        Arrays.fill(primes, true);
-        for(int i = 2; i*i<=primes.length; i++){
-            if(primes[i]){
-                for(int j= i*i; j<primes.length; j=i+j){
-                    primes[j] = false;
+        for(int i = 2; i*i<=prime.length-1; i++){
+            if(prime[i]){
+                for(int j = i*i; j<=prime.length-1; j = j+i){
+                    prime[j] = false;
                 }
             }
         }
-        for(int i = left ; i<= right; i++){
-            count  = isSetBit(i);
-            if(isPrime(count)){
-                ans++;
-            }
-        }
-        return ans;
-    }
-
-    public boolean isPrime(int num){
-        if(num<=1) return false;
-        return primes[num];
-    }
-
-    public int isSetBit(int num){
-        
-        int count = 0;
-        while(num>0){
-            int bit = num%2;
-            if(bit==1){
+        for(int i=left; i<=right; i++){
+            int setBit = isSetBit(i);
+            if(isPrime(setBit)){
                 count++;
             }
-            num /= 2;
+        }
+        return count;
+    }
+
+    public boolean isPrime(int i) {
+        if (i < 2) return false;
+        return prime[i];
+    }
+
+    public int isSetBit(int i) {
+        int count = 0;
+
+        while (i > 0) {
+            if (i % 2 == 1) {
+                count++;
+            }
+            i = i / 2;
         }
         return count;
     }
