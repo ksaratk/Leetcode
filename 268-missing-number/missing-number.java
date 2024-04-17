1e617ddc7136e00df4bffa791385f7b56d4d55da
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-17 19:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (18.98%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,20 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int missing = 0;
-		// for(int i=0; i<nums.length; i++) {
-		// 	max= Math.max(max, nums[i]);
-		// }
-		int [] temp = new int[nums.length+1];
-		for(int i=0; i<nums.length; i++) {
-			temp[nums[i]] = 1;
-		}
-		
-		for(int i=1; i<temp.length; i++) {
-			if(temp[i]==0) {
-				missing = i;
-			}
-		}
-        return missing;
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+        }
+        
+        int n =nums.length+1;
+        n = n*(n-1)/2;
+        return Math.abs(sum-n);
     }
 }
