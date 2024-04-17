f50c42a90e454de69295da74610bee1b7011658e
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-18 00:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (46.68%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,38 +1,33 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-         boolean ans = false;
-        int n = nums.length-1;
-        int left = 0;
-        int right = n;
-
-        while(left<=right){
-            int mid = (right+left)/2;
-
-            if(nums[mid] == target){
-                ans = true;
-                break;
-            }  
-
-            if(nums[left] == nums[mid] && nums[mid] == nums[right]) {
-            left = left+1;
-                right = right-1;
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+         
+            if(nums[mid] == target) return true;
+            
+        if(nums[low] == nums[mid] && nums[mid] == nums[high]) {
+            low = low+1;
+                high = high-1;
             continue;
         }
-
-            if(nums[left] <= nums[mid]){
-                if(target>= nums[left] && target<= nums[mid]){
-                right = mid-1;
-            } else{
-                left = mid+1;
+            
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] &&target<=nums[mid]){
+                    high = mid - 1;
+                } else{
+                    low = mid + 1;
+                }
+            }else{
+                if(target>=nums[mid] &&target<=nums[high]){
+                    low = mid + 1;
+                } else{
+                    high = mid - 1;
+                }
             }
-        } else{
-            if(target>= nums[mid] && target<= nums[right]){
-                left = mid+1;
-            } else{
-                right = mid-1;
-            }
-        } 
-    }
-    return ans;
+        }
+        return false;
     }
 }
