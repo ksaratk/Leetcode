5be1b9f53d52aba23627dbd3c4ad93cff34d206a
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-23 17:28:29 +0530

    Time: 19 ms (69.37%) | Memory: 57.5 MB (78.85%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,44 +1,42 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        
-        int ans = -1;
+        if((m*k)>bloomDay.length) return -1;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
-        for(int i=0; i<bloomDay.length; i++){
+        for(int i = 0; i<bloomDay.length; i++){
             min = Math.min(min, bloomDay[i]);
             max = Math.max(max, bloomDay[i]);
         }
-       int low  = min;
-       int high = max;
-
-       while(low<=high){
-           int mid = (low+high)/2;
-
-           if(ifBouquetForm(mid, k, m, bloomDay)==true){
-               ans = mid;
-               high = mid-1;
-           } else{
-               low = mid+1;
-           }
-       }
+        int ans = -1;
+        int low = min;
+        int high = max;
+      while(low<=high){
+          int mid = (low+high)/2;
+          int count = bouquetCount(bloomDay, mid, k);
+          
+          if(count >= m){
+              ans = mid;
+              high = mid - 1;
+          } else{
+              low =  mid + 1;
+          }
+      }
+        
         return ans;
     }
-
-    public boolean ifBouquetForm(int day, int k, int m, int[] bloomDay){
-        int bouquetForm = 0;
-        int bloomed = 0;
+    
+    public int bouquetCount(int [] bloomDay, int day, int k){
+        int count = 0; int bouquete = 0;
+        
         for(int i=0; i<bloomDay.length; i++){
             if(day>=bloomDay[i]){
-                bloomed++;
-            }else{
-                bouquetForm += bloomed/k;
-                bloomed = 0;
+                count++;
+            }else if(count!=0){
+                bouquete += count/k;
+                count = 0;
             }
         }
-        bouquetForm += bloomed/k;
-        System.out.println(bouquetForm);
-        if(bouquetForm>=m) return true;
-        return false;
-    }   
+        bouquete += count/k;
+        return bouquete;
+    }
 }
