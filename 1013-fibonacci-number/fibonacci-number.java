093a6619ed72b6bc94783ca2960be038d1686123
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-28 16:26:47 +0530

    Time: 8 ms (48.05%) | Memory: 40.4 MB (24.68%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,8 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) return n;
-      return findFib(n);   
-    }
-    
-    public int findFib(int n){
-        if(n == 1 || n == 0){
-            return n;
-        }
+        if(n<2) return n;
         
-     return findFib(n-1) + fib(n-2);
+      return fib(n-1) + fib(n-2);   
     }
+
 }
