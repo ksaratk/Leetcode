4a8fa092495c68fc14bacefb84f093dc26eadcd3
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-04-29 13:49:27 +0530

    Time: 3 ms (99.82%) | Memory: 63.3 MB (75.15%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,17 +10,17 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode fast = head;
+        if(head == null || head.next == null) return null;
         ListNode slow = head;
+        ListNode fast = head;
         ListNode prev = null;
-        if(head.next == null) return null;
+        
         while(fast!=null && fast.next!=null){
             fast = fast.next.next;
             prev = slow;
             slow = slow.next;
         }
-
-        prev.next = prev.next.next;
+        prev.next = slow.next;
         return head;
     }
 }
