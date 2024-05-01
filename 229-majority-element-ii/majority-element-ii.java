8f0e56f5f755e2db353e00709953c12da1a960f9
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-01 19:34:51 +0530

    Time: 2 ms (99.46%) | Memory: 48.1 MB (35.47%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-          int count1=0; 
-          int count2=0;
-          int ele1=Integer.MIN_VALUE;
-          int ele2=Integer.MIN_VALUE;
-      
-          for(int i=0; i<nums.length; i++){
-                if(count1==0 && nums[i]!=ele2){
-                   count1++;
-                   ele1 = nums[i];
-                } else if(count2==0 && nums[i]!=ele1){
-                   count2++;
-                   ele2 = nums[i];
-                } else if(ele1==nums[i]){
-                     count1++;
-                } else if(ele2==nums[i]){
-                     count2++;
-                }else{
-                  count1--;
-                  count2--;
-                }
-          }
-
-          count1 = 0;
+        
+        // List<Integer> list = new  ArrayList<Integer>();
+        int count1 = 0;
+        int count2 = 0;
+        int ele1 = -1;
+        int ele2 = -1;
+        
+        for(int i = 0; i<nums.length; i++){
+            if(count1 == 0 && nums[i] != ele2){
+                ele1 = nums[i];
+                count1++;
+            } else if(count2 == 0 && nums[i] != ele1){
+                ele2 = nums[i];
+                count2++;
+            } else if(ele1 == nums[i]){
+                count1++;
+            }  else if(ele2 == nums[i]){
+                count2++;
+            } else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
           count2 = 0;
 
           for(int i=0; i<nums.length; i++){
