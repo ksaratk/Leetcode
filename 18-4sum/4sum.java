6ddf0c40442244c50f9a8313a32dff6e50a3c617
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-02 02:55:15 +0530

    Time: 636 ms (5.02%) | Memory: 45.5 MB (13.16%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,33 +1,24 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>>  result = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-           if(i>0 && nums[i]==nums[i-1]) continue;
-
-            for(int j=i+1; j<nums.length; j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-
-            int k = j+1;
-            int l = nums.length-1;
-            while(k<l){
-                long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
-                if(sum>target){
-                    l--;
-                } else if(sum<target){
-                    k++;
-                }else{
-                   List<Integer> list =  Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
-                   result.add(list);
-                   k++;
-                   l--;
-                    
-                   while(nums[k]==nums[k-1] && k<l){k++;}
-                   while(nums[l]==nums[l+1] && k<l){l--;}
+        Set<List<Integer>> set = new HashSet<>();
+        int n  = nums.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                Set<Long> tempSet = new HashSet<>();
+                for(int k=j+1; k<n; k++){
+                        long cal = (long)nums[i] + nums[j] + nums[k];
+                        long sum = target - cal;
+                        if(tempSet.contains(sum)){
+                            List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k], (int)sum);
+                            list.sort(null);
+                            set.add(list);
+                        }else{
+                            tempSet.add((long)nums[k]);
+                        }
                 }
             }
-        } 
-    }
-        return result; 
+        }
+        List<List<Integer>>  list = new ArrayList<>(set);
+        return list;
     }
 }
