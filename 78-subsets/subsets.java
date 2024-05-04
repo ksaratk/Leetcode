cec4268a7fa29bb0525b38966336323e6724e2ec
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-04 23:37:28 +0530

    Time: 1 ms (59.66%) | Memory: 42.4 MB (85.79%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,17 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList();
-
-        ans.add(new ArrayList<Integer>());
-
-        for(int val : nums){
-            int n = ans.size();
-
-            for(int i = 0; i < n; i++){
-                List<Integer> list = new ArrayList<>(ans.get(i));
-                list.add(val);
-                ans.add(list);
-            } 
-        }
-        return ans;
+    public List<List<Integer>> subsets(int[] ar) {
+		List<List<Integer>> answer = new ArrayList<>();
+		answer.add(Arrays.asList());
+		
+		for(int i=0; i<ar.length; i++){
+			List<List<Integer>> list = new ArrayList<>();
+			for(int j = 0; j<answer.size(); j++) {
+				List<Integer> temp = new ArrayList<>(answer.get(j));
+			    temp.add(ar[i]);
+			    list.add(temp);
+			}
+			answer.addAll(list);
+		}
+        return answer;
     }
 }
