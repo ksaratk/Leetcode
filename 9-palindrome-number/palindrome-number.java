39aa01722d191aa70aa52f6298c4d7b33018e30a
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-11 20:55:49 +0530

    Time: 4 ms (100.00%) | Memory: 43.7 MB (71.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        if(x<0) return false;
         int newNumber = reverse(x);
         if(newNumber == x){
             return true;
