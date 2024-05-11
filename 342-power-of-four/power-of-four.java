8dcbba4225396b531bc3f06bcf37327de4d1e4fc
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-11 20:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (42.38%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==0) return false;
-        else if(n==1) return true;
+        if(n == 0) return false;
+        else if(n == 1) return true;
         else if(n%4 != 0) return false;
         
-        n = n/4;
-        return isPowerOfFour(n);
+        return isPowerOfFour(n/4);
     }
 }
