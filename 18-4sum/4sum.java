28fcb48aaa6c2baad84ace99f0e629719dd8fa0c
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-15 11:25:18 +0530

    Time: 17 ms (68.50%) | Memory: 43.5 MB (94.21%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        int n  = nums.length;
         Arrays.sort(nums);
-        for(int i=0; i<n; i++){
-            if(i!=0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1; j<n; j++){
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1; j<nums.length; j++){
                 if(j>i+1 && nums[j] == nums[j-1]) continue;
                 int k = j+1;
-                int l = n-1;
+                int n = nums.length - 1;
                 
-                while(k<l){
-                    long sum = (long)nums[i] + nums[j] + nums[k];
-                         sum += (long)nums[l];
-                if(sum>target){
-                    l--;
-                }else if(sum<target){
-                    k++;
-                } else {
-                    List<Integer> tempList = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
-                    list.add(tempList);
-                    l--;
-                    k++;
-                    
-                    while(nums[k] == nums[k-1] && k<l) k++;
-                    while(nums[l] == nums[l+1] && k<l) l--;
+                while(k<n){
+                    long x = nums[i] + nums[j];
+                    long sum = x + nums[k] + nums[n];
+                        
+                    if(sum > target){
+                        n--;
+                    } else if(sum < target){
+                        k++;
+                    } else {
+                        
+                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k], nums[n]);
+                        set.add(list);
+                        k++;
+                        n--;
+                        
+                        while(k<n && nums[k]==nums[k-1]) k++;
+                        while(k<n && nums[n]==nums[n+1]) n--;
+                    }
                 }
-              }
             }
         }
-        return list;
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
     }
 }
