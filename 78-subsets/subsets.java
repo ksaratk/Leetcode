ab5105e2cb182eed540dfa2184705abc831835fb
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-23 02:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (24.98%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList());
-        
-        for(int i=0; i<nums.length; i++){
-            int size = ans.size();
-            for(int j=0; j<size; j++){
-                 List<Integer> temp = new ArrayList<>(ans.get(j));
-                 temp.add(nums[i]);
-                 ans.add(temp);
-            }
-        }
+        List<Integer> list = new ArrayList<>();
+        helper(nums, ans, 0, list);
         return ans;
+    }
+    
+    public void helper(int [] nums, List<List<Integer>> ans, int index, List<Integer> list){
+        ans.add(new ArrayList(list));
+        
+        for(int i = index; i<nums.length; i++){
+            list.add(nums[i]);
+            helper(nums, ans, i + 1, list);
+            list.remove(list.size()-1);
+        }
     }
 }
