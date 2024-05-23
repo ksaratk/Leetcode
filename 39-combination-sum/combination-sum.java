c5bb9dd80e57ff1ef0344cdfad6a8e1353331e3c
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-23 16:01:52 +0530

    Time: 2 ms (85.26%) | Memory: 44.3 MB (85.41%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(candidates);
-        int sum = 0;
+        int index = 0;
         List<Integer> list = new ArrayList<>();
-        helper(ans, list, sum, target, candidates, 0);
+        helper(candidates, ans, index, target, 0, list);
         return ans;
     }
     
-    public void helper(List<List<Integer>> ans, List<Integer> list, int sum, int target, int[] ar, int start){
-        if(sum == target ){
-            ans.add(new ArrayList<>(list));
-            return;
+    public void helper(int [] cand, List<List<Integer>> ans, int index, int target, int sum, List<Integer> list){
+        if(sum == target){
+            ans.add(new ArrayList(list));
         }
         
-        if(sum>target){
-            return;
-        }
+        if(sum > target) return;
         
-        for(int i = start; i<ar.length; i++){
-            if(i>0 && ar[i] == ar[i-1]) continue;
-            sum += ar[i];
-            list.add(ar[i]);
-            helper(ans, list, sum, target, ar, i);
-            sum -= ar[i];
+        for(int i= index; i<cand.length; i++){
+            sum += cand[i];
+            list.add(cand[i]);
+            helper(cand, ans, i, target, sum, list);
+            sum -= cand[i];
             list.remove(list.size()-1);
         }
     }
