2dfd4e725260d938090ddbc9f8e1460584fa08e0
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-25 08:49:29 +0530

    Time: 5 ms (41.14%) | Memory: 44.9 MB (21.46%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,63 +1,59 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
         boolean [][] board = new boolean[n][n];
-        List<List<String>> list = new ArrayList<List<String>>();
-        helper(board, list, 0);
-        return list;
+        helper(0, board, ans);
+        return ans;
     }
     
-    public void helper(boolean[][] board, List list, int r){
-        if(r==board.length){
-            display(board, list);
-            return;
-        }
+    public void helper(int row, boolean [][] board, List<List<String>> ans ){
+           if(row == board.length){
+               display(board, ans);
+               return;
+           }
         
-        for(int c=0; c<board.length; c++){
-            if(isSafe(board, r, c)){
-                board[r][c] = true;
-                helper(board, list, r+1);
-                board[r][c] = false;
+        for(int col = 0; col<board.length; col++){
+            if(isSafe(board, row, col)){
+                board[row][col] = true;
+                helper(row +1 , board, ans);
+                board[row][col] = false;
             }
         }
     }
     
-    public boolean isSafe(boolean [][] board, int r, int c){
-        for(int i=0; i<r; i++){
-            if(board[i][c]){
-                return false;
-            }
+    
+    public boolean isSafe(boolean[][] board, int row, int col){
+        for(int i=0; i<row; i++){
+            if(board[i][col]) return false;
         }
         
-        int maxLeft = Math.min(r,c);
+        int maxLeft= Math.min(row, col);
         for(int i=1; i<=maxLeft; i++){
-            if(board[r-i][c-i]){
-                return false;
-            }
+            if(board[row-i][col-i]) return false;
         }
         
-        int maxRight = Math.min(r, board.length-1-c);
+        int maxRight= Math.min(row, board.length-col-1);
         for(int i=1; i<=maxRight; i++){
-            if(board[r-i][c+i]){
-                return false;
-            }
+            if(board[row-i][col+i]) return false;
         }
+        
         return true;
     }
     
-    public void display(boolean[][]board, List list){
-         int n = board.length;
-        List<String> newList = new ArrayList<String>();
-         for(int i=0; i<board.length; i++){
-             String s = "";
-            for(int j=0; j<board.length; j++){
-               if(board[i][j]){
-                   s = s+"Q";
-               }else{
-                   s = s+".";
-               } 
+    
+    public void display(boolean [][] board, List<List<String>> ans){
+        List<String> temp = new ArrayList<>();
+        for(boolean[] row : board){
+            String s = "";
+            for(boolean col : row){
+                if(col){
+                    s += "Q";
+                } else{
+                    s += ".";
+                }
             }
-             newList.add(s);   
+            temp.add(s);
         }
-        list.add(newList);
+        ans.add(temp);
     }
 }
