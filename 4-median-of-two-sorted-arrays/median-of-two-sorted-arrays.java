76a0ff199b6c8aeb9e7d06d97c8be826169f9552
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-05-26 04:56:45 +0530

    Time: 12 ms (8.78%) | Memory: 46.3 MB (7.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,42 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1  = nums1.length;
-        int n2  = nums2.length;
-        int [] temp = new int[n1 + n2];
-
-        int i =0;
-        int j =0;
-        int k =0;
-       while(i<n1 && j<n2){
-           if(nums1[i] < nums2[j]){
-               temp[k] = nums1[i];
-               k++;
-               i++;
-           } else{
-               temp[k] = nums2[j] ;
-               k++;
-               j++;
-           }
-       }
-
-       while(i<n1){
-            temp[k] = nums1[i];
-               k++;
-               i++;
-       }
-
-       while(j<n2){
-           temp[k] = nums2[j];
-               k++;
-               j++;
-       }
-        int n = temp.length;
-        System.out.println(n);
-       if(n%2==1) {
-           return  temp[n/2];
-       } 
-          double result = ((double) temp[n/2] + (double) temp[(n/2)-1])/2.0;
-        //    ((double) arr3.get(n / 2) + (double) arr3.get((n / 2) - 1)) / 2.0;
-        return result;
+        int i = 0;
+        int j = 0;
+        int n = nums1.length;
+        int m = nums2.length;
+        int m1 = 0;
+        int m2 = 0;
+        int k = 0;
+        while(k <= (n + m)/2){
+            m2 = m1;
+            if(i<n && j<m){
+                if(nums1[i] < nums2[j]){
+                    m1 = nums1[i];
+                    i++;
+                } else{
+                    m1 = nums2[j];
+                    j++;
+                }
+            }else if(i<n){
+                m1 = nums1[i];
+                    i++;
+            }else {
+                m1 = nums2[j];
+                    j++;
+            }
+            k++;
+        }
+        
+        if((n+m)%2 == 1) return (double)m1;
+        System.out.println(m1 +  " " + m2);
+        double ans =  (double)m1+(double)m2;
+        return ans/2.0;
     }
+    
 }
