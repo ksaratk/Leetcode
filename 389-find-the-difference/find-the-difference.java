a487481ebb84bb1e9bb98b2eb61ce3d35e1bbc79
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-06-11 14:59:40 +0530

    Time: 12 ms (12.17%) | Memory: 41.9 MB (23.69%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -3,16 +3,19 @@ class Solution {
        Map<Character, Integer> map = new HashMap<>();
         
         for(int i=0; i<s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
         }
         
         for(int i=0; i<t.length(); i++){
-            map.put(t.charAt(i),  map.getOrDefault(t.charAt(i), 0)-1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)-1);
         }
         
-        for(char ch : map.keySet()){
-            if(map.get(ch) != 0) return ch;
+        for(char key : map.keySet()){
+            if(map.get(key)!=0){
+                return key;
+            }
         }
-        return 'a';
+        
+        return '0';
     }
 }
