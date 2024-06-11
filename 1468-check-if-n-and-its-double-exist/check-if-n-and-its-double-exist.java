776884d620b382d58639f553a3e94fff11478295
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-06-11 22:01:31 +0530

    Time: 2 ms (94.32%) | Memory: 42.2 MB (96.93%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for(int i=0; i<arr.length-1; i++){
+        for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
-                if(arr[i] == (2 * arr[j]) || (2 * arr[i]) == arr[j]){
+                if(arr[i] == 2*arr[j] || arr[j] == 2*arr[i]){
                     return true;
                 }
             }
         }
-                   return false;
+        return false;
     }
 }
