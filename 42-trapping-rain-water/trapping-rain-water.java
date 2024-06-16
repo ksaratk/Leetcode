c70242c3c34e180513e2c789282ff82d634dccd8
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-06-16 19:18:17 +0530

    Time: 1 ms (70.06%) | Memory: 46.1 MB (53.13%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,25 +1,24 @@
 class Solution {
     public int trap(int[] height) {
         int count = 0;
-        for(int i=0; i<height.length; i++){
-            int min = Math.min(maxLeft(height, i), maxRight(height, i));
-            count += min - height[i];
+        int n = height.length;
+        int [] prifix = new int[n];
+        int [] sufix = new int[n];
+        int max = 0;
+        for(int i=0; i<n; i++){
+          max = Math.max(max, height[i]);
+          prifix[i] = max;
+        }
+        
+        int max2 = 0;
+        for(int i=n-1; i>=0; i--){
+          max2 = Math.max(max2, height[i]);
+          sufix[i] = max2;
+        }
+        
+        for(int i=0; i<n; i++){
+            count += Math.min(prifix[i], sufix[i]) - height[i];
         }
         return count;
     }
-    public int maxLeft(int[] arr, int i){
-            int max = arr[i];
-            for(int k=i; k>=0; k--){
-                max = Math.max(max, arr[k]);
-            }
-        return max;
-        }
-     public int maxRight(int[] arr, int i){
-            int max = arr[i];
-            for(int k=i; k<arr.length; k++){
-                max = Math.max(max, arr[k]);
-            }
-         return max;
-        }
-    
 }
