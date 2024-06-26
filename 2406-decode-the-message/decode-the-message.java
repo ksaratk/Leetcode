da87a7851c949f24ea669f9df95bf62def80cf0e
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-06-26 20:33:17 +0530

    Time: 10 ms (43.91%) | Memory: 45.2 MB (7.39%) - LeetSync

--- 2406-decode-the-message/decode-the-message.java
+++ 2406-decode-the-message/decode-the-message.java
@@ -1,22 +1,16 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        Map<Character, Character> map = new HashMap<>();
-        key = key.replaceAll(" ", "").trim();
-        int i = 0;
+        Map<Character, Character> map = new HashMap();
+        int i = 97;
+        map.put(' ', ' ');
         for(char ch : key.toCharArray()){
-            if(!map.containsKey(ch)){
-                 map.put(ch, (char) ('a' + i));
-                 i++;
+            if(ch != ' ' && !(map.containsKey(ch))){
+                map.put(ch, (char)i++);                
             }
         }
-        
         String decode = "";
         for(char ch : message.toCharArray()){
-            if(ch == ' ') {
-                decode += " ";
-            } else {
-                decode += map.get(ch);
-            }
+            decode += map.get(ch);
         }
         return decode;
     }
