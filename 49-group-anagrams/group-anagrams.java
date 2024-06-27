305556a587e9602e42d5ad614a8fde78f9caf3ed
ksaratk <144666810+ksaratk@users.noreply.github.com>
2024-06-27 13:54:52 +0530

    Time: 6 ms (97.93%) | Memory: 47.7 MB (45.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,24 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for(String s : strs){  //  O(n)
-           int [] count = new int[26];
-            for(char ch : s.toCharArray()){
-                count[ch - 'a']++;
-            }
-            
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i<26; i++){
-                sb.append(count[i]);
-                sb.append("$");
-            }
-            String key = sb.toString();
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+        List<List<String>> ans = new ArrayList<>();
+        Map<String, List<String>> m = new HashMap();
+        for (String s : strs){
+            char [] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String s2 = new String(ch);
+            m.putIfAbsent(s2, new ArrayList<String>());
+            m.get(s2).add(s);
         }
-        return new ArrayList<>(map.values());
+        
+        for(Map.Entry<String, List<String>> entry : m.entrySet()){
+            ans.add(entry.getValue());
+        }
+        return ans;
     }
 }
-
-// O(n*k*log k)
